c8d1e26972452eff3ec68f4d0237ab8faf380ec8
Ronniel <piscoster@gmail.com>
2018-01-11 06:53:09 +0000

    readme

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    
+    
+    public function comment()
+    {
+        return $this->hasOne('App\Comment');
+    }
 }
--- database/migrations/2018_01_11_064444_create_comments_table.php
+++ database/migrations/2018_01_11_064444_create_comments_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTasksTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->timestamps();
@@ -26,6 +26,6 @@ class CreateTasksTable extends Migration
      */
     public function down()
     {
-        Schema::drop('tasks');
+        Schema::drop('comments');
     }
 }
